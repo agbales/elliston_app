3a60135dc153f4a60697baac6f72166ff1148527
Andrew Bales <agbales@gmail.com>
2017-04-28 16:35:35 -0700

    Set default format to JSON

--- app/controllers/api/records_controller.rb
+++ app/controllers/api/records_controller.rb
@@ -1,7 +1,13 @@
 class Api::RecordsController < ApplicationController
+  before_action :set_default_format
+
   def show
     @record= Record.find(params[:id])
   end
 
   private
+
+  def set_default_format
+    request.format = :json
+  end
 end
